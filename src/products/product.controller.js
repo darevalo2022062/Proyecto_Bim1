ffa861e78a75d045a663b956e47f97e12c753944
David <darevalo-2022062@kinal.edu.gt>
2024-02-25 17:14:27 -0600

    Existencia única producto

--- src/products/product.controller.js
+++ src/products/product.controller.js
@@ -2,9 +2,17 @@ import Product from './product.js';
 
 export const productPost = async (req, res) => {
     const { nombre, detalles, categoria, stock } = req.body;
-    const product = new Product({ nombre, detalles, categoria, stock });
-    await product.save();
+    var product = null;
+    const existsF = await Product.findOne({ nombre: nombre, estado: false });
 
+    if (existsF) {
+        await Product.findByIdAndUpdate(existsF._id,
+            { $set: { detalles: detalles, categoria: categoria, stock: stock, estado: true } });
+        product = await Product.findById(existsF._id);
+    } else {
+        product = new Product({ nombre, detalles, categoria, stock });
+        await product.save();
+    }
     res.status(200).json({
         msg: "Product Created Successfully",
         product
