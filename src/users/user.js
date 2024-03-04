fb0d1a0d084ba2d1c6cc9ab284597201587e5c89
David <Darevalo-2022062@kinal.edu.gt>
2024-03-04 08:51:00 -0600

    modelo user

---
+++ src/users/user.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = Schema({
+    userName: {
+        type: String,
+        required: [true, 'The "userName" data is mandatory']
+    },
+    email: {
+        type: String,
+        required: [true, 'The "email" data is mandatory']
+    },
+    password: {
+        type: String,
+        required: [true, 'The "pssword" data is mandatory']
+    },
+    role: {
+        type: String,
+        required: [true, 'The "pssword" data is mandatory']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+export default model("User", UserSchema);
